a26917ed9d150a6cf6932afcec7e789321348d58
Jatin Hasija <hasijajatin02@gmail.com>
2026-02-10 18:18:29 +0530

    serverless

--- api/get-url.js
+++ api/get-url.js
@@ -10,18 +10,49 @@ export default async function handler(req, res) {
     if (!username) {
       return res.status(400).json({ success: false, error: "Username is required" });
     }
+    const SF_PRIVATE_KEY_CONTENT = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDAb9VpMLVyEvN3
+l91x5ZzlwsY0tbUrR0+nNE+nMafTENNLVUgzqS3E97djJ6lWFxenwJlqhASgN6zc
+Lim6bVRcagn3F5wJQdHdr+pcEmo7SKYI3WgIT8/2AfGzeIVxB5PS2TecE4qMfdfM
+aGD9D3YO//pTv2A9dS6S8hLzvw8PXcLx8scFEV3b98jdjG+7cTik6mgpcA9QAdxr
+Vozbw4suCBYNsxm4+9RzxNpb54x+RCpnjKt4mbsAbdipA5TO6rGHfEht2sVKHSdf
+hmmQ3+9OnpmYDtqsDJdjc0nUWMEBK3n6k8hifj3lOSDQV3yL12W2k8wtrn/WmAmn
+PVDYtOXzAgMBAAECggEATt0GDsfhdFCD6p0YKZc6B4cdB3j4ODZPVGzBv/k/l+Yr
+UZIvtsw8b549T6tvYFKyZRMvTFGyO57Vfp6Eh1xK2Fy0nMjWL40D3uA7IWSCmK0b
+LkLeYZmj1mPRlAVuWnYKhLx0Z8gyXYUtLR4RsWPmtBr/kda6MDWX0qzWS4IfP+Ir
+7Ud1m6tGENouxGgb3WC1bDf2IJBbN6n5+u6V5REMllO3nXT8xuKWpglYGLrqBQha
+KKBPPsjzdQxP543K7Xpbh9TOyMLGtYprY4Fcb5/AJ03/4+5/u5nCQ2wwAOcqOqmR
+/j4tXuq8/M4tXiF7e18s2kJPWfrqTJX7EMlyUFgjAQKBgQD4jra3TyRKJQX70LAb
+SL6nTNST4MqRFFYClfIjLyutz6UNAT73L37kYnkRFusZN1SMSlzO4fbuQmo2QT/p
+KWnrkbHQoLcAcwZPNF1zfCj1iBGTZmlzwS8AnbXbMqUkPJTiIMbxR/AAJG4tmy6Q
+c45s6YJUjVLw5uQK4WWsPGCDwQKBgQDGMu8rws3uI8sMkDKqJGFerywD+ZYv+na0
+vzdgqR1eJY52QCqbGBZEh96NfIu46mFp6Nqct1i3wO7dj03pkh9eorqtgawwvClF
+ChQC5qzf0gtm5v2mHG9PPGd7R3u6LyJvSTkanX/2ulabBKyJ1bqky3mTQ+rkXQcr
+3nx44bJGswKBgQC8OW66gC8uCr1uRUmnT744+SCd34Pmu6yqhpx38ne0RuNpZsEk
+9Q21q7CctWtRjCn1lzaS4GFbamX+MglQ6zCv2IP8RkhvCawEfNYr5yEpVmJg6hP/
+OZL7d3LIzKBsQYhJeaLOArkHko7w8GwhrbB0X5x3pRHjJiYY6zlQZeR8wQKBgHaK
+bivRNlNxCvd3oyXJ3oa/5GZ4N5B4j9jo6NkiLJNriPpGxHTAgkj4rm4XCIZPgHXU
+tDO5IRG+Dd9r32CMacCzOU2e2frH/zLwpSlbCBhRy0n/a8OXDCfq68V66pBfsou2
+0rWNb8SEjFnpALzrnNcFb7BZlbqBvVFKn025R2QrAoGAOyTQrNTX261/h+dbe+wf
+uwyytLCbnrxGUNtfpk3mrdeZm92PWMvIjw8zD+G26vkGO5FILI6gCRf3OdTqpOqU
+XIyhXRhekoj53krOTfBD1IR7cRRFrqFT8IekjKG/7Bw+1vU8ymqR2U6PUYpF8SbZ
+0051VAlaS/uUCLg8Aspy6kk=
+-----END PRIVATE KEY-----
+`
+    const SF_CLIENT_ID = '3MVG9VMBZCsTL9hnVO_6Q8ke.y8YH_PdvEaFgNFzWAELOYeUzsvkX3EwBJA31A1wP4MvYR.lFL4VsZpbWLd2H';
+    const SF_LOGIN_URL = 'https://login.salesforce.com'
 
     // 1. Fix Private Key: Vercel environment variables often mangle newlines.
-    const rawKey = process.env.SF_PRIVATE_KEY_CONTENT;
+    const rawKey = SF_PRIVATE_KEY_CONTENT;
     if (!rawKey) throw new Error("SF_PRIVATE_KEY_CONTENT is missing in Vercel settings");
     const privateKey = rawKey.replace(/\\n/g, '\n');
 
     /* ================= 2. Create JWT Assertion ================= */
     const jwtToken = jwt.sign(
       {
-        iss: process.env.SF_CLIENT_ID,     // Consumer Key from Connected App
+        iss: SF_CLIENT_ID,     // Consumer Key from Connected App
         sub: username,                    // Salesforce Username
-        aud: process.env.SF_LOGIN_URL,    // https://login.salesforce.com or test.salesforce.com
+        aud: SF_LOGIN_URL,    // https://login.salesforce.com or test.salesforce.com
         exp: Math.floor(Date.now() / 1000) + 300
       },
       privateKey,
@@ -29,7 +60,7 @@ export default async function handler(req, res) {
     );
 
     /* ================= 3. Get Access Token ================= */
-    const sfRes = await fetch(`${process.env.SF_LOGIN_URL}/services/oauth2/token`, {
+    const sfRes = await fetch(`${SF_LOGIN_URL}/services/oauth2/token`, {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams({
